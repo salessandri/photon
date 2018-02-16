d58d5842ff4d03dc7e5194ca11b5a56bec4a5518
Santiago Alessandri <san.lt.ss@gmail.com>
2018-02-15 22:07:55 -0800

    Add state persistence to the app

--- App.js
+++ App.js
@@ -5,20 +5,37 @@ import { applyMiddleware, createStore } from 'redux'
 // Logger with default options
 import logger from 'redux-logger'
 
+import { persistStore, persistReducer } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
+import storage from 'redux-persist/lib/storage'
+import hardSet from 'redux-persist/lib/stateReconciler/hardSet'
+
 import photonApp from './reducers'
 
 import PhotonAppContainer from './containers/PhotonAppContainer';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  stateReconciler: hardSet,
+}
+
+const persistedReducer = persistReducer(persistConfig, photonApp)
+
 let store = createStore(
-  photonApp,
+  persistedReducer,
   applyMiddleware(logger)
 )
+
+let persistor = persistStore(store)
 
 export default class App extends React.Component {
   render () {
     return (
       <Provider store={store}>
-        <PhotonAppContainer />
+        <PersistGate loading={null} persistor={persistor}>
+          <PhotonAppContainer />
+        </PersistGate>
       </Provider>
     )
   }
