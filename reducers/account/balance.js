15e882c87907a6dfeb60f40716de318067f2dc90
Santiago Alessandri <san.lt.ss@gmail.com>
2018-03-25 15:53:08 -0700

    Properly handle TX fees in the balance.
    Remove the account merge operation handling

--- reducers/account/balance.js
+++ reducers/account/balance.js
@@ -1,4 +1,6 @@
 
+import { BigDecimal } from 'bigdecimal'
+
 const initialState = {
   balanceOperations: [],
   currentBalance: '0.0000000',
@@ -17,23 +19,73 @@ const createAccountReducer = (state, accountId, op) => {
       currentBalance: balanceOperation.amount
     }
   }
+  else if (accountId === op.sourceAccount) { // This account is the one paying to open the account
+    let operationAmount = new BigDecimal(op.startingBalance).negate()
+    let newBalance = new BigDecimal(state.currentBalance).add(operationAmount)
+
+    let balanceOperation = {
+      operationRef: op.id,
+      date: op.createdAt,
+      amount: operationAmount.toPlainString()
+    }
+    return {
+      ...state,
+      balanceOperations: [...state.balanceOperations, balanceOperation],
+      currentBalance: newBalance.toPlainString()
+    }
+  }
+  return state
+}
+
+const accountMergeReducer = (state, accountId, op) => {
+  if (accountId === op.account) { // This is account being created
+    let balanceOperation = {
+      operationRef: op.id,
+      date: op.createdAt,
+      amount: op.startingBalance
+    }
+    return {
+      ...state,
+      balanceOperations: [...state.balanceOperations, balanceOperation],
+      currentBalance: balanceOperation.amount
+    }
+  }
+  return state
+}
+
+const xlmFromStroops = stroops => {
+  return new BigDecimal(stroops).movePointLeft(7).setScale(7)
+}
+
+const addTransactionReducer = (state, accountId, tx) => {
+  if (accountId === tx.sourceAccount) {
+    // Need to pay the tx fees
+    let operationAmount = xlmFromStroops(tx.fee).negate()
+    let newBalance = new BigDecimal(state.currentBalance).add(operationAmount)
+
+    let balanceOperation = {
+      trasnactionRef: tx.id,
+      date: tx.createdAt,
+      amount: operationAmount.toPlainString()
+    }
+    return {
+      ...state,
+      balanceOperations: [...state.balanceOperations, balanceOperation],
+      currentBalance: newBalance.toPlainString()
+    }
+  }
+  return state
 }
 
 const balanceReducer = (state = initialState, action) => {
   switch (action.type) {
+    // Transaction side effects on the balance
+    case 'ADD_TRANSACTION':
+      return addTransactionReducer(state, action.accountId, action.transaction)
     case 'ADD_CREATE_ACCOUNT_OPERATION':
       return createAccountReducer(state, action.accountId, action.operation)
     case 'ADD_ACCOUNT_MERGE_OPERATION':
-      let balanceOperation = {
-        operationRef: action.operation.id,
-        date: action.operation.createdAt,
-        amount: action.operation.startingBalance
-      }
-      return {
-        ...state,
-        balanceOperations: [...state.balanceOperations, balanceOperation],
-        currentBalance: balanceOperation.amount
-      }
+      return state
     default:
       return state
   }
