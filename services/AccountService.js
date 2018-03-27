25ce7e8f2c34743f85549da0dec6921861e56552
Santiago Alessandri <san.lt.ss@gmail.com>
2018-03-26 23:26:06 -0700

    Integrate the operation service into the app
    Remove the operation handling code from the AccountService

--- services/AccountService.js
+++ services/AccountService.js
@@ -1,7 +1,5 @@
 import { Connect } from 'redux-ddd';
 
-import StellarNetworkService from './StellarNetworkService'
-
 import {
   addTransaction,
   addCreateAccountOperation,
@@ -16,10 +14,18 @@ import {
   addManageDataOperation
 } from '../actions'
 
+import StellarNetworkService from './StellarNetworkService'
+import OperationService from './OperationService';
+
 @Connect(state => ({
   accounts: state.accounts,
 }))
 class AccountService {
+
+  constructor() {
+    this._stellarNetworkService = StellarNetworkService
+    this._operationService = OperationService
+  }
 
   // The onAction listener is called every time an action is
   // dispatched in the Redux store. It can be helpful when we
@@ -40,15 +46,14 @@ class AccountService {
   }
 
   _startUpdateStream(accountId, operationsCursor, txCursor) {
-    let operationsStream = StellarNetworkService.getServer()
+    let operationsStream = this._stellarNetworkService.getServer()
       .operations()
       .forAccount(accountId)
       .cursor(operationsCursor)
       .stream({
-        onmessage: op => { this._processOperation(accountId, op) },
-        onerror: err => { this._processOperationError(accountId, err) }
+        onmessage: op => { this._operationService.processOperation(accountId, op) }
       })
-    let transactionsStream = StellarNetworkService.getServer()
+    let transactionsStream = this._stellarNetworkService.getServer()
       .transactions()
       .forAccount(accountId)
       .cursor(txCursor)
@@ -76,245 +81,6 @@ class AccountService {
     }
     let addTxAction = addTransaction(accountId, modelTx)
     this.dispatch(addTxAction)
-  }
-
-  _processOperation(accountId, operation) {
-    switch (operation.type) {
-      case 'create_account':
-        this._processCreateAccount(accountId, operation)
-        return
-      case 'payment':
-        this._processPayment(accountId, operation)
-        return
-      case 'path_payment':
-        this._processPathPayment(accountId, operation)
-        return
-      case 'manage_offer':
-        this._processManageOffer(accountId, operation)
-        return
-      case 'create_passive_offer':
-        this._processCreatePassiveOffer(accountId, operation)
-        return
-      case 'set_options':
-        this._processSetOptions(accountId, operation)
-        return
-      case 'change_trust':
-        this._processChangeTrust(accountId, operation)
-        return
-      case 'allow_trust':
-        this._processAllowTrust(accountId, operation)
-        return
-      case 'account_merge':
-        this._processAccountMerge(accountId, operation)
-        return
-      case 'inflation':
-        this._processInflation(accountId, operation)
-        return
-      case 'manage_data':
-        this._processManageData(accountId, operation)
-        return
-    }
-  }
-
-  _processOperationError(err) {
-    console.log('Operation stream error: ' + JSON.stringify(err))
-  }
-
-  _processCreateAccount(accountId, operation) {
-    let modelOp = {
-      id: operation.id,
-      sourceAccount: operation.source_account,
-      type: operation.type,
-      createdAt: operation.created_at,
-      transactionId: operation.transaction_hash,
-      startingBalance: operation.starting_balance,
-      funder: operation.funder,
-      account: operation.account
-    }
-    let action = addCreateAccountOperation(accountId, modelOp)
-    this.dispatch(action)
-  }
-
-  _processPayment(accountId, operation) {
-    let modelOp = {
-      id: operation.id,
-      sourceAccount: operation.source_account,
-      type: operation.type,
-      createdAt: operation.created_at,
-      transactionId: operation.transaction_hash,
-      assetType: operation.asset_type,
-      assetCode: operation.asset_code,
-      assetIssuer: operation.asset_issuer,
-      from: operation.from,
-      to: operation.to,
-      amount: operation.amount
-    }
-    let action = addPaymentOperation(accountId, modelOp)
-    this.dispatch(action)
-  }
-
-  _processPathPayment(accountId, operation) {
-    let modelOp = {
-      id: operation.id,
-      sourceAccount: operation.source_account,
-      type: operation.type,
-      createdAt: operation.created_at,
-      transactionId: operation.transaction_hash,
-      from: operation.from,
-      to: operation.to,
-      sourceAssetType: operation.source_asset_type,
-      sourceAssetIssuer: operation.source_asset_issuer,
-      sourceAssetCode: operation.source_asset_code,
-      sourceMaxAmount: operation.source_max,
-      sourceAmount: operation.source_amount,
-      destinationAssetType: operation.asset_type,
-      destinationAssetIssuer: operation.asset_issuer,
-      destinationAssetCode: operation.asset_code,
-      destinationAmount: operation.amount
-    }
-    let action = addPathPaymentOperation(accountId, modelOp)
-    this.dispatch(action)
-  }
-
-  _processManageOffer(accountId, operation) {
-    let modelOp = {
-      id: operation.id,
-      sourceAccount: operation.source_account,
-      type: operation.type,
-      createdAt: operation.created_at,
-      transactionId: operation.transaction_hash,
-      offerId: operation.offer_id,
-      amount: operation.amount,
-      buyingAssetType: operation.buying_asset_type,
-      buyingAssetIssuer: operation.buying_asset_issuer,
-      buyingAssetCode: operation.buying_asset_code,
-      price: operation.price,
-      priceRatio: operation.price_r,
-      sellingAssetType: operation.selling_asset_type,
-      sellingAssetIssuer: operation.selling_asset_issuer,
-      sellingAssetCode: operation.selling_asset_code
-    }
-    let action = addManageOfferOperation(accountId, modelOp)
-    this.dispatch(action)
-  }
-
-  _processCreatePassiveOffer(accountId, operation) {
-    let modelOp = {
-      id: operation.id,
-      sourceAccount: operation.source_account,
-      type: operation.type,
-      createdAt: operation.created_at,
-      transactionId: operation.transaction_hash,
-      offerId: operation.offer_id,
-      amount: operation.amount,
-      buyingAssetType: operation.buying_asset_type,
-      buyingAssetIssuer: operation.buying_asset_issuer,
-      buyingAssetCode: operation.buying_asset_code,
-      price: operation.price,
-      priceRatio: operation.price_r,
-      sellingAssetType: operation.selling_asset_type,
-      sellingAssetIssuer: operation.selling_asset_issuer,
-      sellingAssetCode: operation.selling_asset_code
-    }
-    let action = addCreatePassiveOfferOperation(accountId, modelOp)
-    this.dispatch(action)
-  }
-
-  _processSetOptions(accountId, operation) {
-    let modelOp = {
-      id: operation.id,
-      sourceAccount: operation.source_account,
-      type: operation.type,
-      createdAt: operation.created_at,
-      transactionId: operation.transaction_hash,
-      signerKey: operation.signer_key,
-      signerWeight: operation.signer_weight,
-      masterKeyWeight: operation.master_key_weight,
-      lowThreshold: operation.low_threshold,
-      mediumThreshold: operation.med_threshold,
-      highThreshold: operation.high_threshold,
-      homeDomain: operation.home_domain,
-      flagsSet: operation.set_flags_s,
-      flagsCleared: operation.clear_flags_s
-    }
-    let action = addSetOptionsOperation(accountId, modelOp)
-    this.dispatch(action)
-  }
-
-  _processChangeTrust(accountId, operation) {
-    let modelOp = {
-      id: operation.id,
-      sourceAccount: operation.source_account,
-      type: operation.type,
-      createdAt: operation.created_at,
-      transactionId: operation.transaction_hash,
-      assetType: operation.asset_type,
-      assetIssuer: operation.asset_issuer,
-      assetCode: operation.asset_code,
-      trustee: operation.trustee,
-      trustor: operation.trustor,
-      limit: operation.limit
-    }
-    let action = addChangeTrustOperation(accountId, modelOp)
-    this.dispatch(action)
-  }
-
-  _processAllowTrust(accountId, operation) {
-    let modelOp = {
-      id: operation.id,
-      sourceAccount: operation.source_account,
-      type: operation.type,
-      createdAt: operation.created_at,
-      transactionId: operation.transaction_hash,
-      assetType: operation.asset_type,
-      assetIssuer: operation.asset_issuer,
-      assetCode: operation.asset_code,
-      trustee: operation.trustee,
-      trustor: operation.trustor,
-      authorize: operation.authorize
-    }
-    let action = addAllowTrustOperation(accountId, modelOp)
-    this.dispatch(action)
-  }
-
-  _processAccountMerge(accountId, operation) {
-    let modelOp = {
-      id: operation.id,
-      sourceAccount: operation.source_account,
-      type: operation.type,
-      createdAt: operation.created_at,
-      transactionId: operation.transaction_hash,
-      account: operation.account,
-      into: operation.into
-    }
-    let action = addAccountMergeOperation(accountId, modelOp)
-    this.dispatch(action)
-  }
-
-  _processInflation(accountId, operation) {
-    let modelOp = {
-      id: operation.id,
-      sourceAccount: operation.source_account,
-      type: operation.type,
-      createdAt: operation.created_at,
-      transactionId: operation.transaction_hash
-    }
-    let action = addInflationOperation(accountId, modelOp)
-    this.dispatch(action)
-  }
-
-  _processManageData(accountId, operation) {
-    let modelOp = {
-      id: operation.id,
-      sourceAccount: operation.source_account,
-      type: operation.type,
-      createdAt: operation.created_at,
-      transactionId: operation.transaction_hash,
-      name: operation.name,
-      value: operation.value
-    }
-    let action = addManageDataOperation(accountId, modelOp)
-    this.dispatch(action)
   }
 
   _updateStreams = {}
