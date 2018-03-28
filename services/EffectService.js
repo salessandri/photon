79af6b2e743f91fab8f6841233f2d10ea960ff4b
Santiago Alessandri <san.lt.ss@gmail.com>
2018-03-27 22:35:40 -0700

    #3: Add parsing for the account_credited effect

--- services/EffectService.js
+++ services/EffectService.js
@@ -29,6 +29,17 @@ class EffectService {
     return this._parseBasicEffect(rawEffect)
   }
 
+  parseAccountCredited(rawEffect) {
+    let basicEffect = this._parseBasicEffect(rawEffect)
+    return {
+      ...basicEffect,
+      assetType: rawEffect.asset_type,
+      assetIssuer: rawEffect.asset_issuer,
+      assetCode: rawEffect.asset_code,
+      amount: rawEffect.amount
+    }
+  }
+
   _parseBasicEffect(rawEffect) {
     return {
       id: rawEffect.id,
--- services/EffectService.test.js
+++ services/EffectService.test.js
@@ -41,3 +41,30 @@ it('"account_removed" effect is parsed correctly', () => {
   let parsedEffect = EffectService.parseAccountRemoved(rawEffect)
   expect(parsedEffect).toEqual(expectedEffect)
 })
+
+it('"account_credited" effect is parsed correctly', () => {
+  let rawEffect = {
+    id: '0034970031745929217-0000000001',
+    paging_token: '34970031745929217-1',
+    account: 'GDZXM6232OJYT6B2IQGFASUJEPPIS5I5XROU7MEHGHKLWFVEJ5UPS4CN',
+    type: 'account_credited',
+    type_i: 2,
+    asset_type: 'credit_alphanum4',
+    asset_code: 'ATN',
+    asset_issuer: 'GCAUZH5OGE4HU4NZPBXX67A66D6DVR2IIZMT2BU635UN5PJXWUPUO3A7',
+    amount: '1.0000000',
+  }
+
+  let expectedEffect = {
+    id: '0034970031745929217-0000000001',
+    account: 'GDZXM6232OJYT6B2IQGFASUJEPPIS5I5XROU7MEHGHKLWFVEJ5UPS4CN',
+    type: 'account_credited',
+    assetType: 'credit_alphanum4',
+    assetIssuer: 'GCAUZH5OGE4HU4NZPBXX67A66D6DVR2IIZMT2BU635UN5PJXWUPUO3A7',
+    assetCode: 'ATN',
+    amount: '1.0000000',
+  }
+
+  let parsedEffect = EffectService.parseAccountCredited(rawEffect)
+  expect(parsedEffect).toEqual(expectedEffect)
+})
